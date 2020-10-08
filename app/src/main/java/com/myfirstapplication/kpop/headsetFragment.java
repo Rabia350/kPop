ab658cb82f0d60d36131546de457f6de14529267
Rabia350 <35rabiaijaz@gmail.com>
2020-10-08 20:15:56 +0500

    listview

--- app/src/main/java/com/myfirstapplication/kpop/headsetFragment.java
+++ app/src/main/java/com/myfirstapplication/kpop/headsetFragment.java
@@ -66,12 +66,11 @@ public class headsetFragment extends Fragment {
 
         final ListView list = getActivity().findViewById(R.id.list);
         ArrayList<SubjectData> arrayList = new ArrayList<SubjectData>();
-        arrayList.add(new SubjectData("JAVA", "https://www.tutorialspoint.com/java/", "https://www.tutorialspoint.com/java/images/java-mini-logo.jpg"));
-        arrayList.add(new SubjectData("Python", "https://www.tutorialspoint.com/python/", "https://www.tutorialspoint.com/python/images/python-mini.jpg"));
-        arrayList.add(new SubjectData("Javascript", "https://www.tutorialspoint.com/javascript/", "https://www.tutorialspoint.com/javascript/images/javascript-mini-logo.jpg"));
-        arrayList.add(new SubjectData("Cprogramming", "https://www.tutorialspoint.com/cprogramming/", "https://www.tutorialspoint.com/cprogramming/images/c-mini-logo.jpg"));
-        arrayList.add(new SubjectData("Cplusplus", "https://www.tutorialspoint.com/cplusplus/", "https://www.tutorialspoint.com/cplusplus/images/cpp-mini-logo.jpg"));
-        arrayList.add(new SubjectData("Android", "https://www.tutorialspoint.com/android/", "https://www.tutorialspoint.com/android/images/android-mini-logo.jpg"));
+        arrayList.add(new SubjectData("Tum Hi Ana", "https://www.youtube.com/watch?v=o4t7EhH5vLs", "https://en.wikipedia.org/wiki/Tum_Hi_Aana#/media/File:Tum_Hi_Aana.jpg"));
+        arrayList.add(new SubjectData("Ap Baithy hain", "https://www.youtube.com/watch?v=kxuI06hQG8k", "https://www.google.com/search?q=ap+baithy+hain+only+image&tbm=isch&ved=2ahUKEwieo_2LoaXsAhW0gHMKHWbGCskQ2-cCegQIABAA&oq=ap+baithy+hain+only+image&gs_lcp=CgNpbWcQA1CvjgVY9tkFYLvcBWgDcAB4AIAB-wSIAagfkgEJMi05LjIuMC4ymAEAoAEBqgELZ3dzLXdpei1pbWfAAQE&sclient=img&ei=Iyl_X96FNLSBzgPmjKvIDA&bih=657&biw=1366&rlz=1C1EJFA_enPK797PK806#imgrc=jicocGfU8PMwcM"));
+        arrayList.add(new SubjectData("Let me Love", "https://www.youtube.com/watch?v=VP5ODDPUpcE", "https://en.wikipedia.org/wiki/Let_Me_Love_You_(DJ_Snake_song)#/media/File:DJSnakeLetMeLoveYou.jpg"));
+        arrayList.add(new SubjectData("MashUp 2019", "https://www.youtube.com/watch?v=Zdo5WW7DZZY", "https://www.google.com/search?q=mashup+arijit+and+atif&tbm=isch&ved=2ahUKEwiXm8uVo6XsAhVPgRoKHdINC-0Q2-cCegQIABAA&oq=mashup+arijit+and+atif&gs_lcp=CgNpbWcQAzoCCAA6BggAEAUQHjoGCAAQCBAeOgQIABAYUNwrWJVjYIVlaABwAHgAgAHzAogB-h6SAQYyLTEwLjSYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=UCt_X5ezNc-CatKbrOgO&bih=657&biw=1366&rlz=1C1EJFA_enPK797PK806#imgrc=sM5dWJDqP8FDvM"));
+        arrayList.add(new SubjectData("Baby Baby", "https://www.youtube.com/watch?v=1a5SWpp9Wfg", "https://www.google.com/search?q=baby+baby+song&tbm=isch&ved=2ahUKEwicl8Gco6XsAhUQfBoKHcB4BM0Q2-cCegQIABAA&oq=baby+baby+song&gs_lcp=CgNpbWcQAzICCAAyAggAMgIIADICCAAyAggAMgIIADICCAAyAggAMgIIADICCAA6BQgAELEDOgcIABCxAxBDOgQIABBDOgQIABAeOgYIABAFEB5QytMIWJudCWCSqAloAHAAeACAAdUFiAGPP5IBCTMtMS4zLjcuM5gBAKABAaoBC2d3cy13aXotaW1nwAEB&sclient=img&ei=Xyt_X9zrF5D4acDxkegM&bih=657&biw=1366&rlz=1C1EJFA_enPK797PK806#imgrc=5z6tV0dUGTQVCM"));
         CustomAdapter customAdapter = new CustomAdapter(getActivity(), arrayList);
         list.setAdapter(customAdapter);
 
